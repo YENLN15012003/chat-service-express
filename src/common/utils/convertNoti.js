3a4a8b4950ed27ac11fa11302376769c108fc8c4
dieutuyetnguyenthi <dieutuyetnguyenthi@gmail.com>
2025-11-17 23:24:57 +0700

    Fix conver noti

--- src/common/utils/convertNoti.js
+++ src/common/utils/convertNoti.js
@@ -1,9 +1,18 @@
 const getFullUserInfo = require("../../services/getFullUserInfor");
+const { TYPE } = require("../../models/Noti");
 
 const convertNoti = async (noti) => {
+  switch (noti.type) {
+    case TYPE.ACCEPT_FRIEND_REQUEST:
+      return converNotiAcceptFriend(noti);
+  }
+};
+
+const converNotiAcceptFriend = async (noti) => {
   const userReferenceEmail = noti.referenceEmail;
   const userReference = await getFullUserInfo(userReferenceEmail);
   console.log("ssssss: ", userReference);
+
   return {
     id: noti._id,
     content: noti.content,
